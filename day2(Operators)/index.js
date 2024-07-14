9d1902fc35e99b0fe9d6927db318dd77b041bea1
Sardar-Saifullah <115546745+Sardar-Saifullah@users.noreply.github.com>
2024-07-15 01:19:54 +0500

    day2 javascript challenge solution

---
+++ day2(Operators)/index.js
@@ -0,0 +1,82 @@
+                        // DAY 2 OPERAROTORS
+                    
+// activity 1 : arithmetic operators
+console.log("Arithmetic Operator Result...");
+// task 1 add two numbers
+let num1 = 96;
+let num2 = 67;
+let sum = num1 + num2;
+console.log(sum);
+
+// task 2 subtract two numbers
+
+let subtract = num1 - num2;
+console.log(subtract);
+
+// task 3 multiply two numbers
+let multiply = num1 * num2;
+console.log(multiply);
+
+// task 4 divide two numbers
+let divide = num1 / num2;
+console.log(divide);
+
+// task 5 remainder of two number
+let remainder = num1 % num2;
+console.log(remainder);
+
+
+// Activity 2 : Assignment Operator
+console.log("assignment operator result...");
+// task 6
+let num3 = 9;
+let num4 = 6;
+num3 += num4;
+console.log(num3);
+// task 7
+num3 -= num4;
+console.log(num3);
+
+// Activity 3 : Comparison Operator
+console.log("comparison operator result....");
+// task 8
+console.log(num1>num3);
+console.log(num4<num2);
+
+// task 9
+console.log(8>=90);
+console.log(5<=5);
+console.log(5<=0);
+
+// task 10
+console.log(5==5);
+console.log(5===5);
+console.log(5=="5");
+console.log(5==="5");
+
+//Logical Operator
+console.log("logical operator result....");
+// task 11
+
+console.log(num1>num2 && num4<num3);
+
+// task12
+console.log(num1<num4|| num1<num2);
+
+// task 13
+console.log(!(num1>num3));
+
+// Activity 5: Ternary Operator
+console.log("ternary operator result....");
+// task 14
+let num5 = -9;
+console.log(num1>0?"positive":"negative");
+console.log(num5>0?"positive":"negative");
+
+
+
+
+
+
+
+
