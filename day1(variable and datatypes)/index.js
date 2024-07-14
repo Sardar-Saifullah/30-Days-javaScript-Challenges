919dec3e2c1677fcfebd4d4222aaee4c8126b3d5
Sardar-Saifullah <115546745+Sardar-Saifullah@users.noreply.github.com>
2024-07-14 21:23:42 +0500

    day1 js challenge solution

---
+++ day1(variable and datatypes)/index.js
@@ -0,0 +1,50 @@
+                                        // DAY 1 : VARIABLES AND DATA TYPES
+// activity 1: Variable and Data Types
+
+ var num = 10;
+ console.log(num);
+ 
+ let str = "javascript challenge";
+ console.log(str);
+
+//  activity 2: constant declaration
+
+ const flag = true;
+ console.log(flag);
+
+//  activity 3: Data types
+
+let num1 = 234;
+console.log(num1);
+console.log(typeof(num1));
+
+let str1 = "javascript";
+console.log(str1);
+console.log(typeof(str1));
+
+let flag1 = false;
+console.log(flag1);
+console.log(typeof(flag1));
+
+let arr1 = ["a","b","c","d","e"];
+console.log(arr1);
+console.log(typeof(arr1));
+
+var obj = {
+    "name":"saifullah",
+    "subject": " javascript challenge day 1",
+    "date":" 14 july 2024"
+};
+console.log(obj);
+console.log(typeof(obj));
+
+// activity 4: reassigning variables
+ let a = 67;
+  a=90;
+  console.log(a);
+
+//   activity 5: understanding const
+const number = 67;
+// number = 90 ;
+console.log(number);
+
